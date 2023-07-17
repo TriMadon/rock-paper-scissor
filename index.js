598cc1295a0c0e52f91dcf9756cc20d3b95b91bf
TriMadon <mehammed6789@gmail.com>
2023-07-17 13:01:58 +0300

    Add CSS and interactivity

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const ROCK = "Rock";
 const PAPER = "Paper";
 const SCISSORS = "Scissors";
 const CHOICES = [ROCK, PAPER, SCISSORS];
-let playerScore = 0, computerScore = 0;
+let playerScore = 0,
+	computerScore = 0;
+let gameEnd = false;
 
 function getComputerChoice() {
 	let choice = CHOICES[Math.floor(Math.random() * 3)];
@@ -13,11 +15,6 @@ function playRound(playerSelection, computerSelection) {
 	let pSel = capitalize(playerSelection);
 	let comSel = computerSelection;
 	let result;
-
-	if (!CHOICES.includes(pSel)) {
-		alert(`Invalid player input! "${pSel}" is not a valid RPS move.`);
-		return;
-	}
 
 	if (pSel === comSel) {
 		result = `It's a Tie! ${pSel} is tied against ${comSel}.`;
@@ -41,35 +38,73 @@ function capitalize(playerSelection) {
 	);
 }
 
-const buttons = document.querySelectorAll("button");
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".button");
 const roundResult = document.querySelector(".results .round");
-const score = document.querySelector(".results .score");
-const gameResult = document.querySelector(".results .game")
+const pScore = document.querySelector(".player .score");
+const comScore = document.querySelector(".computer .score");
+const gameResult = document.querySelector(".results .game");
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
 	button.addEventListener("click", () => {
-		let result = playRound(button.id, getComputerChoice())
+		let playerChoice = button.id;
+		let computerChoice = getComputerChoice()
+		let result = playRound(playerChoice, computerChoice);
 		roundResult.textContent = result;
+		roundResult.className = "round";
+		if (result.includes("Win")) {
+			roundResult.classList.add("roundWon");
+		}
+		if (result.includes("Lose")) {
+			roundResult.classList.add("roundLost");
+		}
+		buttons.forEach((button) => button.className = "button");
+		if (capitalize(playerChoice) === computerChoice) {
+			button.classList.add("bothChosen");
+		} else {
+			button.classList.add("chosen");
+		}
+		buttons.forEach((button) => {
+			if(capitalize(button.id) === computerChoice){
+				button.classList.add("compChosen");
+			}
+		})
 		updateScore(result);
-	})
-})
+	});
+});
 
 function updateScore(result) {
-	if (playerScore === 0 && computerScore === 0) {
-		gameResult.textContent = "";
+	if (gameEnd) {
+		resetGame();
+		return;
 	}
 	playerScore += result.includes("Win") ? 1 : 0;
 	computerScore += result.includes("Lose") ? 1 : 0;
-	score.textContent = `You: ${playerScore}  Computer: ${computerScore}`;
+	pScore.textContent = playerScore;
+	comScore.textContent = computerScore;
 	if (playerScore === 5) {
-		gameResult.textContent = "You won the game! game ends."
+		gameResult.textContent =
+			"You won the game! click any button to restart.";
+		body.classList.add("gameWon");
 	}
 	if (computerScore === 5) {
-		gameResult.textContent = "You lost the game! game ends."
+		gameResult.textContent =
+			"You lost the game! click any button to restart.";
+		body.classList.add("gameLost");
 	}
-	if (playerScore === 5 || computerScore === 5){
+	if (playerScore === 5 || computerScore === 5) {
 		resetScores();
+		gameEnd = true;
 	}
+}
+
+function resetGame() {
+	body.className = "";
+	gameResult.textContent = "";
+	pScore.textContent = "";
+	comScore.textContent = "";
+	roundResult.textContent = "";
+	gameEnd = false;
 }
 
 function resetScores() {
